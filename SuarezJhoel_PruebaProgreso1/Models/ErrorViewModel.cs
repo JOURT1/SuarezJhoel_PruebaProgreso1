db5e0ae357684744444966dcf154857b4984d5be
Crhystel <crhystelvelasco213@gmail.com>
2024-10-29 07:29:37 -0500

    Agregar archivos de proyecto.

---
+++ SuarezJhoel_PruebaProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuarezJhoel_PruebaProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
