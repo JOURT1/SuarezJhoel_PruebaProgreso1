df5f65676be628be9542d90ba3e28ac19e740dd2
jourt1 <jhoelsuarez02@gmail.com>
2024-10-29 07:43:33 -0500

    Commit 1: Modelos

---
+++ SuarezJhoel_PruebaProgreso1/Models/Celular.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuarezJhoel_PruebaProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Modelo { get; set; }
+        public DateOnly año { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
---
+++ SuarezJhoel_PruebaProgreso1/Models/JSuarez.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace SuarezJhoel_PruebaProgreso1.Models
+{
+    public class JSuarez
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [AllowNull]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Range(0, 100)]
+        public decimal Edad { get; set; }
+
+        public Boolean Donante_de_organos { get; set; }
+
+        public DateOnly FechaNaciemiento { get; set; }
+
+    }
+}
